1672faa9401a1441a9b33a6b5e96d63799ad2edb
mdsujon-mridha <mdsujan134523@gmail.com>
2022-02-12 21:15:24 +0600

    Admin

---
+++ home.js
@@ -0,0 +1,19 @@
+
+// change image from box-1 
+
+document.getElementById('img1').onmouseover = function() 
+{
+    document.getElementById("img1").src = "images/images-4.jpg"; 
+}
+// change image from box-2 
+
+document.getElementById('img2').onmouseover = function() 
+{
+    document.getElementById("img2").src = "images/images-5.jpg"; 
+}
+
+// change image from box-3 
+document.getElementById('img3').onmouseover = function() 
+{
+    document.getElementById("img3").src = "images/images-6.jpg"; 
+}
